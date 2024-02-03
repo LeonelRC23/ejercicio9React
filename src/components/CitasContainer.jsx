f78bda60a51eaa659a787a844e2ed8bec4b84c2b
Cordero Leonel Rodrigo <leonelrcordero22@gmail.com>
2024-02-03 00:34:07 -0300

    Cambios en algunos textos e implementacion de borrado de citas

--- src/components/CitasContainer.jsx
+++ src/components/CitasContainer.jsx
@@ -1,25 +1,33 @@
-import React, { useEffect, useState } from "react";
-import "../styles/citasContainer.css";
-import CitaCard from "./CitaCard";
-import PrincipalForm from "./PrincipalForm";
+import React, { useEffect, useState } from 'react';
+import '../styles/citasContainer.css';
+import CitaCard from './CitaCard';
+import PrincipalForm from './PrincipalForm';
 
 const CitasContainer = () => {
   const [almacenamientoCitas, setAlmacenamientoCitas] = useState([]);
   useEffect(() => {
-    setAlmacenamientoCitas(JSON.parse(localStorage.getItem("consulta")) || []);
+    // almacenamientoCitas = JSON.parse(localStorage.getItem('consulta'));
+    setAlmacenamientoCitas(JSON.parse(localStorage.getItem('consulta')) || []);
   }, []);
   let deleteCard = (indice) => {
-    const citasFilter = almacenamientoCitas.filter((cita) => cita !== indice);
-    setAlmacenamientoCitas(citasFilter);
-  }
+    // console.log(almacenamientoCitas);
+    const citasFilter = almacenamientoCitas.filter((cita) => cita !== indice); //generar un id para poder eliminar las citas
+    // setAlmacenamientoCitas(citasFilter);
+    console.log(citasFilter);
+  };
   return (
-    <div className="my-5 citasContainer">
-      <div className="stateCitasContainer">
-        <p className="text-center m-0 p-4 citasText">No hay citas</p>
+    <div className='my-5 citasContainer'>
+      <div className='stateCitasContainer'>
+        <p className='text-center m-0 p-4 citasText'>Citas pendientes</p>
       </div>
-      <div className="contentContainer row m-0">
+      <div className='contentContainer row m-0'>
         {almacenamientoCitas.map((objeto, indice) => (
-          <CitaCard key={indice} citaDatos={objeto} indice={indice} deleteCard={deleteCard}/>
+          <CitaCard
+            key={indice}
+            citaDatos={objeto}
+            indice={indice}
+            deleteCard={deleteCard}
+          />
         ))}
       </div>
     </div>
--- src/components/PrincipalForm.jsx
+++ src/components/PrincipalForm.jsx
@@ -1,61 +1,61 @@
-import React, { useEffect, useRef, useState } from "react";
-import CitasContainer from "./CitasContainer";
-import { Button, Form } from "react-bootstrap";
-import { Formik } from "formik";
-import "../styles/principalForm.css";
+import React, { useEffect, useRef, useState } from 'react';
+import CitasContainer from './CitasContainer';
+import { Button, Form } from 'react-bootstrap';
+import { Formik } from 'formik';
+import '../styles/principalForm.css';
 
 const PrincipalForm = () => {
   const [almacenamientoState, setAlmacenamientoState] = useState([]);
   let valuesForm = {
-    nombreMascota: "",
-    nombreDueño: "",
-    fecha: "",
-    Hora: "",
-    sintomas: "",
+    nombreMascota: '',
+    nombreDueño: '',
+    fecha: '',
+    Hora: '',
+    sintomas: '',
   };
   useEffect(() => {
-    setAlmacenamientoState(JSON.parse(localStorage.getItem("consulta")) || []);
+    setAlmacenamientoState(JSON.parse(localStorage.getItem('consulta')) || []);
   }, []);
   return (
-    <div className="principalDiv">
-      <div className="p-4 textContainer">
-        <p className="m-0">Llenar el formulario para crear una cita.</p>
+    <div className='principalDiv'>
+      <div className='p-4 textContainer'>
+        <p className='m-0'>Llenar el formulario para crear una cita.</p>
       </div>
       <div>
         <Formik
           initialValues={valuesForm}
           validate={(values) => {
             let errores = {};
-            if (values.nombreMascota.trim() === "") {
-              errores.nombreMascota = "Campo requerido.";
+            if (values.nombreMascota.trim() === '') {
+              errores.nombreMascota = 'Campo requerido.';
             } else if (!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(values.nombreMascota)) {
-              errores.nombreMascota = "Ingrese nombre valido.";
+              errores.nombreMascota = 'Ingrese nombre valido.';
             }
-            if (values.nombreDueño.trim() === "") {
-              errores.nombreDueño = "Campo requerido.";
+            if (values.nombreDueño.trim() === '') {
+              errores.nombreDueño = 'Campo requerido.';
             } else if (!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(values.nombreDueño)) {
-              errores.nombreDueño = "Ingrese un nombre valido.";
+              errores.nombreDueño = 'Ingrese un nombre valido.';
             }
-            if (values.sintomas.trim() === "") {
-              errores.sintomas = "Campo requerido.";
+            if (values.sintomas.trim() === '') {
+              errores.sintomas = 'Campo requerido.';
             }
-            if (values.fecha === "") {
-              errores.fecha = "Campo requerido.";
+            if (values.fecha === '') {
+              errores.fecha = 'Campo requerido.';
             }
-            if (values.Hora === "") {
-              errores.Hora = "Campo requerido.";
+            if (values.Hora === '') {
+              errores.Hora = 'Campo requerido.';
             }
             return errores;
           }}
           onSubmit={(valores, { resetForm }) => {
             resetForm();
             const almacenamiento =
-              JSON.parse(localStorage.getItem("consulta")) || [];
+              JSON.parse(localStorage.getItem('consulta')) || [];
             if (almacenamiento.length == 0) {
-              localStorage.setItem("consulta", JSON.stringify([valores]));
+              localStorage.setItem('consulta', JSON.stringify([valores]));
             } else {
               almacenamiento.push(valores);
-              localStorage.setItem("consulta", JSON.stringify(almacenamiento));
+              localStorage.setItem('consulta', JSON.stringify(almacenamiento));
             }
             window.location.reload(); //mala practica
           }}
@@ -70,104 +70,103 @@ const PrincipalForm = () => {
             touched,
           }) => (
             <Form onSubmit={handleSubmit}>
-              <div className="formContainer p-4">
-                <Form.Group className="d-flex py-3">
-                  <Form.Label className="m-0 labelLarge">
-                    Nombre de la mascota:{" "}
+              <div className='formContainer p-4'>
+                <Form.Group className='d-flex py-3'>
+                  <Form.Label className='m-0 labelLarge'>
+                    Nombre de la mascota:{' '}
                   </Form.Label>
-                  <div className="w-100">
+                  <div className='w-100'>
                     <Form.Control
-                      type="text"
-                      id="nombreMascota"
-                      name="nombreMascota"
-                      placeholder="Nombre de la mascota"
-                      className="p-0 px-3 inputLarge"
+                      type='text'
+                      id='nombreMascota'
+                      name='nombreMascota'
+                      placeholder='Nombre de la mascota'
+                      className='p-0 px-3 inputLarge'
                       onBlur={handleBlur}
                       onChange={handleChange}
                     />
                     {/* Como hacer que el div ocupe e tamaño del texto de error asi no este cambiando de tamaño el contenedor */}
                     {touched.nombreMascota && errors.nombreMascota && (
-                      <div className="px-3 text-danger">
+                      <div className='px-3 text-danger'>
                         {errors.nombreMascota}
                       </div>
                     )}
                   </div>
                 </Form.Group>
-                <Form.Group className="d-flex py-3">
-                  <Form.Label className="m-0 labelLarge">
+                <Form.Group className='d-flex py-3'>
+                  <Form.Label className='m-0 labelLarge'>
                     Nombre del dueño:
                   </Form.Label>
-                  <div className="w-100">
+                  <div className='w-100'>
                     <Form.Control
-                      type="text"
-                      id="nombreDueño"
-                      name="nombreDueño"
-                      placeholder="Nombre del dueño"
-                      className="p-0 px-3 inputLarge"
+                      type='text'
+                      id='nombreDueño'
+                      name='nombreDueño'
+                      placeholder='Nombre del dueño'
+                      className='p-0 px-3 inputLarge'
                       onBlur={handleBlur}
                       onChange={handleChange}
                     />
                     {touched.nombreDueño && errors.nombreDueño && (
-                      <div className="px-3 text-danger">
+                      <div className='px-3 text-danger'>
                         {errors.nombreDueño}
                       </div>
                     )}
                   </div>
                 </Form.Group>
-                <Form.Group className="d-flex py-3">
-                  <Form.Label className="m-0 labelSmall">Fecha:</Form.Label>
-                  <div className="w-100">
+                <Form.Group className='d-flex py-3'>
+                  <Form.Label className='m-0 labelSmall'>Fecha:</Form.Label>
+                  <div className='w-100'>
                     <Form.Control
-                      type="date"
-                      id="fecha"
-                      name="fecha"
-                      placeholder="dd/mm/yyyy"
+                      type='date'
+                      id='fecha'
+                      name='fecha'
+                      placeholder='dd/mm/yyyy'
                       onBlur={handleBlur}
                       onChange={handleChange}
                     />
                     {touched.fecha && errors.fecha && (
-                      <div className="px-3 text-danger">{errors.fecha}</div>
+                      <div className='px-3 text-danger'>{errors.fecha}</div>
                     )}
                   </div>
-                  <Form.Label className="m-0 labelSmall text-center">
+                  <Form.Label className='m-0 labelSmall text-center'>
                     Hora:
                   </Form.Label>
-                  <div className="w-100">
+                  <div className='w-100'>
                     <Form.Control
-                      type="time"
-                      id="Hora"
-                      name="Hora"
-                      placeholder="hh:mm"
+                      type='time'
+                      id='Hora'
+                      name='Hora'
+                      placeholder='hh:mm'
                       onBlur={handleBlur}
                       onChange={handleChange}
                     />
                     {touched.Hora && errors.Hora && (
-                      <div className="px-3 text-danger">{errors.Hora}</div>
+                      <div className='px-3 text-danger'>{errors.Hora}</div>
                     )}
                   </div>
                 </Form.Group>
-                <Form.Group className="d-flex py-3">
-                  <Form.Label className="m-0 labelLarge">Sintomas: </Form.Label>
-                  <div className="w-100">
+                <Form.Group className='d-flex py-3'>
+                  <Form.Label className='m-0 labelLarge'>Sintomas: </Form.Label>
+                  <div className='w-100'>
                     <Form.Control
-                      className="inputLarge"
-                      type="text"
-                      id="sintomas"
-                      name="sintomas"
-                      placeholder="Describir sintomas"
+                      className='inputLarge'
+                      type='text'
+                      id='sintomas'
+                      name='sintomas'
+                      placeholder='Describir sintomas'
                       onBlur={handleBlur}
                       onChange={handleChange}
                     />
                     {touched.sintomas && errors.sintomas && (
-                      <div className="px-3 text-danger">{errors.sintomas}</div>
+                      <div className='px-3 text-danger'>{errors.sintomas}</div>
                     )}
                   </div>
                 </Form.Group>
               </div>
-              {/* el formulario no deveria enviarse cuando los inputs estan vacios pero lo hace  */}
-              <div className="formButton p-4 text-center">
-                <Button className="boton" type="submit">
-                  <p className="my-1 mx-4">Agregar nueva cita</p>
+              <div className='formButton p-4 text-center'>
+                <Button className='boton' type='submit'>
+                  <p className='my-1 mx-4'>Agregar nueva cita</p>
                 </Button>
               </div>
             </Form>
